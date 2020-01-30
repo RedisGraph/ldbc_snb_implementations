1179981441a917401a690aba7337da493b8e03e2
filipecosta90 <filipecosta.90@gmail.com>
2020-01-30 12:55:15 +0000

    [wip] wip on making neo client working

--- cypher/configure-neo4j.sh
+++ cypher/configure-neo4j.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+source ./environment-variables-neo4j.sh
 
 $NEO4J_HOME/bin/neo4j-admin set-initial-password admin
--- cypher/environment-variables-neo4j.sh
+++ cypher/environment-variables-neo4j.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-NEO4J_HOME=${NEO4J_HOME:-"${HOME}/redislabs/ldbc_snb_implementations/cypher/neo4j-server"}
+NEO4J_HOME=${NEO4J_HOME:-"${HOME}/redislabs/ldbc_snb_implementations/cypher/neo4j-server/neo4j-community-3.3.6"}
 NEO4J_DB_DIR=${NEO4J_DB_DIR:-"${NEO4J_HOME}/data/databases/graph.db"}
 DATAGEN_DIR=${DATAGEN_DIR:-"${HOME}/redislabs/ldbc_snb_datagen"}
 NEO4J_DATA_DIR=${NEO4J_DATA_DIR:-"${DATAGEN_DIR}/social_network"}
